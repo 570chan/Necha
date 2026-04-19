f022d24631eec577bc62ec0c16e6118710f56fbe
Chan <570channee@gmail.com>
2026-04-19 07:23:23 +0700

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+react
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx' // Nạp code từ file App.jsx của bạn
+import './index.css'     // Nạp các style cơ bản (Tailwind)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
